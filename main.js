cd499212cf819c7f771ede5761637e41f88d1f5a
leannel22 <leannel@andrew.cmu.edu>
2021-11-28 21:02:13 -0500

    Created main.js for javascript

---
+++ main.js
@@ -0,0 +1,9 @@
+function load_details() {
+    console.log("yo");
+    // check_cart();
+    // let bun_temp = localStorage.getItem("bun");
+    // document.getElementById("prod_name").innerHTML = `${cinnabons[bun_temp]["name"]} Cinnamon Roll`
+    // document.getElementById("prod_img").src = `${cinnabons[bun_temp]["photo"]}`
+    // document.getElementById("prod_ingr").innerHTML = `${cinnabons[bun_temp]["ingredients"]}`
+    // `` Back ticks will allow JS to put variables in HTML
+}
