580c471a1185d5bfd69ceeb5a167931adc0b78d5
leannel22 <leannel@andrew.cmu.edu>
2021-11-28 21:11:01 -0500

    Created data.js file

---
+++ data.js
@@ -0,0 +1,18 @@
+var spring_produce = { 
+    "peas": {
+        name: "Peas", 
+        photo: "background/backgroundSpring.png"
+    },
+    "rhubarb": {
+        name: "Rhubarb", 
+        photo: "background/backgroundSpring.png"
+    },
+    "spinach": {
+        name: "Spinach", 
+        photo: "background/backgroundSpring.png" 
+    },
+    "asparagus": {
+        name: "Asparagus", 
+        photo: "background/backgroundSpring.png"
+    }
+}
